0c65076e7f4bf3ae8da977aa4c767805f9f14b00
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-05-12 18:30:11 -0500

    gitignore fixed

--- models/Workout.js
+++ models/Workout.js
@@ -2,38 +2,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    string: {
-      type: String,
-      trim: true,
-      required: "String is Required"
+    day: {
+        type:Date, 
     },
+    
+    exercises: [
+        {
+            type: String
+        },
+        {
+            name: String
+        },
+        {
+            duration: Number
+        },
+        {
+            weight: Number
+        },
+        {
+            reps: Number
+        },
+        {
+            sets: Number
+        },
+    ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
   
-    number: {
-      type: Number,
-      unique: true,
-      required: true
-    },
-  
-    email: {
-      type: String,
-      match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
-    },
-  
-    boolean: Boolean,
-  
-    array: Array,
-  
-    date: {
-      type: Date,
-      default: Date.now
-    },
-  
-    longstring: {
-      type: String,
-      validate: [({ length }) => length >= 6, "Longstring should be longer."]
-    }
-  });
-  
-  const Workout = mongoose.model("Workout", WorkoutSchema);
-  
-  module.exports = Example;
+module.exports = Workout;
